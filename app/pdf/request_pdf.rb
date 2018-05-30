0e8891a93d0031da95cd8283d4ee7e3aefc02933
polken <genesis62@hotmail.com>
2018-05-30 20:17:58 +0000

    seems finished the pdf lets see!

--- app/pdf/request_pdf.rb
+++ app/pdf/request_pdf.rb
@@ -5,14 +5,14 @@ class RequestPdf < Prawn::Document
         super()
         @request = request
         printpage1
+        printpage2
     end
     
     private
         def printpage1()
-            
-            Prawn::Font::AFM.hide_m17n_warning = true
-			imgdif = "#{Rails.root.to_s}/app/assets/images/diflog.jpg"
 		#------------------------------
+		    Prawn::Font::AFM.hide_m17n_warning = true
+			imgdif = "#{Rails.root.to_s}/app/assets/images/diflog.jpg"
 			image imgdif, at: [17,720], fit:[120,120]
 			photo = "#{Rails.root.to_s}/public/#{@request.person.picture.to_s}"
 			image photo, at: [260,667], fit:[80,80]
@@ -135,7 +135,7 @@ class RequestPdf < Prawn::Document
     	    ], at:[317,360], width:480, height:30
     	    
     	    table_relationships
-    	    printLines
+    	    printLines_Page1
         end
         
         def table_relationships
@@ -175,7 +175,7 @@ class RequestPdf < Prawn::Document
             end
         end
         
-        def printLines
+        def printLines_Page1
             stroke do
                 self.line_width = 0.5
                 horizontal_line 382,530, at:659 #no exp
@@ -213,7 +213,168 @@ class RequestPdf < Prawn::Document
                 horizontal_line 395,530, at:352 # edad
                 
                 rounded_rectangle [5,340],530,330,4
+    
             end
+        end
+        
+        def printLines_Page2
+            stroke do
+                self.line_width = 0.5
+                horizontal_line 100,285, at:702
+                horizontal_line 430,530, at:702
+                
+                horizontal_line 74,285, at:687
+                horizontal_line 338,530, at:687
+                
+                horizontal_line 83,285, at:672
+                horizontal_line 347,530, at:672
+                
+                horizontal_line 70,285, at:657
+                horizontal_line 370,530, at:657
+                
+                horizontal_line 43,285, at:642
+                horizontal_line 330,530, at:642
+                
+                horizontal_line 70,285, at:627
+                horizontal_line 340,530, at:627
+                
+                horizontal_line 67,285, at:612
+                horizontal_line 43,285, at:597
+                horizontal_line 220,380, at:572
+                rounded_rectangle [17,565],530,20,8
+                rounded_rectangle [17,540],530,20,8
+                rounded_rectangle [17,565],530,20,8
+                rounded_rectangle [17,520],530,90,8
+                
+                vertical_line 540,430, at:145
+                rectangle [112,515],15,60
+                horizontal_line 112,127, at:500
+                horizontal_line 112,127, at:485
+                horizontal_line 112,127, at:470
+                
+                vertical_line 540,430, at:270
+                rectangle [235,515],15,75
+                horizontal_line 235,250, at:500
+                horizontal_line 235,250, at:485
+                horizontal_line 235,250, at:470
+                horizontal_line 235,250, at:455
+                
+                vertical_line 540,430, at:420
+                rectangle [365,515],15,60
+                horizontal_line 365,380, at:500
+                horizontal_line 365,380, at:485
+                horizontal_line 365,380, at:470
+                
+                rectangle [510,515],15,60
+                horizontal_line 510,525, at:500
+                horizontal_line 510,525, at:485
+                horizontal_line 510,525, at:470
+                
+                rounded_rectangle [17,420],530,20,8
+                rounded_rectangle [17,400],530,90,8
+                vertical_line 420,310, at:199
+                vertical_line 420,310, at:381
+                
+                rectangle [150,388],15,60
+                horizontal_line 150,165, at:373
+                horizontal_line 150,165, at:358
+                horizontal_line 150,165, at:343
+                
+                rectangle [315,388],15,60
+                horizontal_line 315,330, at:373
+                horizontal_line 315,330, at:358
+                horizontal_line 315,330, at:343
+                
+                rectangle [480,388],15,60
+                horizontal_line 480,495, at:373
+                horizontal_line 480,495, at:358
+                horizontal_line 480,495, at:343
+                
+                horizontal_line 100,545, at:273
+                horizontal_line 17,545, at:253
+                horizontal_line 17,545, at:233
+                horizontal_line 17,545, at:213
+                horizontal_line 17,545, at:193
+                horizontal_line 17,545, at:173
+                horizontal_line 17,545, at:153
+                horizontal_line 17,545, at:133
+                horizontal_line 17,545, at:113
+                horizontal_line 17,545, at:93
+                horizontal_line 17,545, at:73
+                
+                horizontal_line 17,209, at:33
+                horizontal_line 350,545, at:33
+            end
+        end
+        
+        def printpage2
+            start_new_page
+            stroke_axis
+            #----------------------------------------------------------------------------------------------------------------------------------------------
+                formatted_text_box [ { :text => "Egresos Mensuales: ", size:9, style:[:bold], color:'000000'} ], at:[17,710], width:120, height:30
+                formatted_text_box [ { :text => "Alimentacion: ", size:9, style:[:bold], color:'000000'} ], at:[17,695], width:120, height:30
+                formatted_text_box [ { :text => "Renta de Casa: ", size:9, style:[:bold], color:'000000'} ], at:[17,680], width:120, height:30
+                formatted_text_box [ { :text => "Electricidad: ", size:9, style:[:bold], color:'000000'} ], at:[17,665], width:120, height:30
+                formatted_text_box [ { :text => "Agua: ", size:9, style:[:bold], color:'000000'} ], at:[17,650], width:120, height:30
+                formatted_text_box [ { :text => "Combustible: ", size:9, style:[:bold], color:'000000'} ], at:[17,635], width:120, height:30
+                formatted_text_box [ { :text => "Educacion: ", size:9, style:[:bold], color:'000000'} ], at:[17,620], width:120, height:30
+                formatted_text_box [ { :text => "Otros: ", size:9, style:[:bold], color:'000000'} ], at:[17,605], width:120, height:30
+                formatted_text_box [ { :text => "PRESTACIONES SOCIALES: ", size:9, style:[:bold], color:'000000'} ], at:[310,710], width:150, height:30
+                formatted_text_box [ { :text => "IMSS: ", size:9, style:[:bold], color:'000000'} ], at:[310,695], width:120, height:30
+                formatted_text_box [ { :text => "ISSSTE: ", size:9, style:[:bold], color:'000000'} ], at:[310,680], width:120, height:30
+                formatted_text_box [ { :text => "PARTICULAR: ", size:9, style:[:bold], color:'000000'} ], at:[310,665], width:120, height:30
+                formatted_text_box [ { :text => "DIF: ", size:9, style:[:bold], color:'000000'} ], at:[310,650], width:120, height:30
+                formatted_text_box [ { :text => "OTRO: ", size:9, style:[:bold], color:'000000'} ], at:[310,635], width:120, height:30
+                formatted_text_box [ { :text => "TOTAL: ", size:10, style:[:bold], color:'000000'} ], at:[180,580], width:120, height:30
+                formatted_text_box [ { :text => "CONDICIONES DE LA VIVIENDA: ", size:10, style:[:bold], color:'000000'} ], at:[200,558], width:180, height:20
+                formatted_text_box [ { :text => "HABITACION", size:10, style:[:bold], color:'000000'} ], at:[50,534], width:80, height:20
+                formatted_text_box [ { :text => "PAREDES", size:10, style:[:bold], color:'000000'} ], at:[187,534], width:80, height:20
+                formatted_text_box [ { :text => "TECHO", size:10, style:[:bold], color:'000000'} ], at:[325,534], width:80, height:20
+                formatted_text_box [ { :text => "PISO ", size:10, style:[:bold], color:'000000'} ], at:[475,534], width:80, height:20
+            #----------------------------------------------------------------------------------------------------------------------------------------------
+                formatted_text_box [ { :text => "PROPIA ", size:9, style:[:bold], color:'000000'} ], at:[40,510], width:80, height:20
+                formatted_text_box [ { :text => "RENTADA ", size:9, style:[:bold], color:'000000'} ], at:[40,495], width:80, height:20
+                formatted_text_box [ { :text => "PRESTADA ", size:9, style:[:bold], color:'000000'} ], at:[40,480], width:80, height:20
+                formatted_text_box [ { :text => "OTROS ", size:9, style:[:bold], color:'000000'} ], at:[40,465], width:80, height:20
+            #----------------------------------------------------------------------------------------------------------------------------------------------
+                formatted_text_box [ { :text => "BLOCK ", size:9, style:[:bold], color:'000000'} ], at:[170,510], width:80, height:20
+                formatted_text_box [ { :text => "LADRILLO ", size:9, style:[:bold], color:'000000'} ], at:[170,495], width:80, height:20
+                formatted_text_box [ { :text => "CARTON ", size:9, style:[:bold], color:'000000'} ], at:[170,480], width:80, height:20
+                formatted_text_box [ { :text => "MADERA ", size:9, style:[:bold], color:'000000'} ], at:[170,465], width:80, height:20
+                formatted_text_box [ { :text => "OTROS ", size:9, style:[:bold], color:'000000'} ], at:[170,450], width:80, height:20
+            #----------------------------------------------------------------------------------------------------------------------------------------------
+                formatted_text_box [ { :text => "CONCRETO ", size:9, style:[:bold], color:'000000'} ], at:[300,510], width:80, height:20
+                formatted_text_box [ { :text => "CARTON ", size:9, style:[:bold], color:'000000'} ], at:[300,495], width:80, height:20
+                formatted_text_box [ { :text => "PALMA ", size:9, style:[:bold], color:'000000'} ], at:[300, 480], width:80, height:20
+                formatted_text_box [ { :text => "OTROS ", size:9, style:[:bold], color:'000000'} ], at:[300,465], width:80, height:20
+            #----------------------------------------------------------------------------------------------------------------------------------------------
+                formatted_text_box [ { :text => "MOSAICO ", size:9, style:[:bold], color:'000000'} ], at:[450,510], width:80, height:20
+                formatted_text_box [ { :text => "CEMENTO ", size:9, style:[:bold], color:'000000'} ], at:[450,495], width:80, height:20
+                formatted_text_box [ { :text => "TIERRA ", size:9, style:[:bold], color:'000000'} ], at:[450, 480], width:80, height:20
+                formatted_text_box [ { :text => "OTROS ", size:9, style:[:bold], color:'000000'} ], at:[450, 465], width:80, height:20
+            #----------------------------------------------------------------------------------------------------------------------------------------------
+                formatted_text_box [ { :text => "ALUMBRADO", size:10, style:[:bold], color:'000000'} ], at:[70,410], width:80, height:20
+                formatted_text_box [ { :text => "SERVICIO DE AGUA", size:10, style:[:bold], color:'000000'} ], at:[240,410], width:100, height:20
+                formatted_text_box [ { :text => "TRANSPORTE", size:10, style:[:bold], color:'000000'} ], at:[435,410], width:80, height:20
+            #----------------------------------------------------------------------------------------------------------------------------------------------
+                formatted_text_box [ { :text => "ELECTRICO ", size:9, style:[:bold], color:'000000'} ], at:[87,385], width:80, height:20
+                formatted_text_box [ { :text => "LAMPARA DE PETROLEO ", size:9, style:[:bold], color:'000000'} ], at:[30,370], width:140, height:20
+                formatted_text_box [ { :text => "VELA ", size:9, style:[:bold], color:'000000'} ], at:[115, 355], width:400, height:20
+                formatted_text_box [ { :text => "OTROS ", size:9, style:[:bold], color:'000000'} ], at:[107,340], width:400, height:20
+            #----------------------------------------------------------------------------------------------------------------------------------------------
+                formatted_text_box [ { :text => "INTERDOMICILIARIA ", size:9, style:[:bold], color:'000000'} ], at:[215,385], width:100, height:20
+                formatted_text_box [ { :text => "PILA ", size:9, style:[:bold], color:'000000'} ], at:[285,370], width:80, height:20
+                formatted_text_box [ { :text => "POZO ", size:9, style:[:bold], color:'000000'} ], at:[280, 355], width:80, height:20
+                formatted_text_box [ { :text => "OTROS ", size:9, style:[:bold], color:'000000'} ], at:[275,340], width:80, height:20
+            #----------------------------------------------------------------------------------------------------------------------------------------------
+                formatted_text_box [ { :text => "PROPIO ", size:9, style:[:bold], color:'000000'} ], at:[435,385], width:80, height:20
+                formatted_text_box [ { :text => "TAXI ", size:9, style:[:bold], color:'000000'} ], at:[450,370], width:80, height:20
+                formatted_text_box [ { :text => "CAMION ", size:9, style:[:bold], color:'000000'} ], at:[435, 355], width:80, height:20
+                formatted_text_box [ { :text => "OTROS ", size:9, style:[:bold], color:'000000'} ], at:[440,340], width:80, height:20
             
+            formatted_text_box [ { :text => "OBSERVACIONES :", size:9, style:[:bold], color:'000000'} ], at:[17,280], width:90, height:10
+            formatted_text_box [ { :text => "FIRMA DEL BENEFICIARIO", size:9, style:[:bold], color:'000000'} ], at:[50,25], width:120, height:10
+            formatted_text_box [ { :text => "FIRMA DEL TRABAJADOR SOCIAL", size:9, style:[:bold], color:'000000'} ], at:[380,25], width:150, height:10
+            printLines_Page2
         end
 end
